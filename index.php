babfde9e860b3df1b9a07e5f044d4af708d80ee0
Leonardo Alaff <110346180+leonardoalaff@users.noreply.github.com>
2025-05-14 21:55:08 -0300

    1.1

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site HC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="menu">
+        <div class="menu-icon menu-icon-perfil">
+            <h1 class="texto-menu texto-menu-perfil">Perfil</h1>
+        </div>
+    </div>
+
+    <header>
+        <h1>Controle de sobras HC</h1>
+    </header>
+
+    <main>
+        
+        <section class="sessao1">
+            <div class="m-plano"></div>
+            <div class="m-naoplano"></div>
+        </section>
+    </main>
+
+    <footer></footer>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
